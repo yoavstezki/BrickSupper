9ecd68a7dd4428b8c8690ae76759c457de2bdf2a
Yoav Stezki <yoavs@pinkpark.com>
2017-09-18 18:46:29 +0300

    Lib GXD generate

---
+++ desktop/src/com/yoavstezki/bricksupper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.yoavstezki.bricksupper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.yoavstezki.bricksupper.BrickSupper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BrickSupper(), config);
+	}
+}
---
+++ html/src/com/yoavstezki/bricksupper/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.yoavstezki.bricksupper.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.yoavstezki.bricksupper.BrickSupper;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new BrickSupper();
+        }
+}
